8a46259a7b8d4f4fc8e12e63512605562917f433
Fatih Firdaus <fatihfirdaus404@gmail.com>
2026-05-08 14:46:51 +0700

    modified:   plugins/misc/misc-enable-disable.js

--- plugins/misc/misc-enable-disable.js
+++ plugins/misc/misc-enable-disable.js
@@ -1,11 +1,9 @@
-import Group from '../../databases/orm/Group.js';
-
 export default {
     command: ['enable', 'disable', 'on', 'off'],
     category: 'group',
     isAdmin: true,
     description: 'Mengaktifkan atau menonaktifkan fitur bot',
-    async execute(sock, m, msgData, user, group) {
+    async execute(sock, m, msgData, _, group) {
         const availableFeatures = ['welcome', 'limit'];
         const feature = msgData.args[0]?.toLowerCase();
         const action = msgData.commandName;
@@ -20,21 +18,21 @@ export default {
         // Fitur khusus Grup
         if (['welcome', 'limit'].includes(feature)) {
             if (!msgData.isGroup) {
-                return sock.sendMessage(msgData.remoteJid, { 
-                    text: `Aduuh! Fitur *${feature}* cuma bisa dipake di dalam grup aja kak~ (｡T ω T｡)` 
+                return sock.sendMessage(msgData.remoteJid, {
+                    text: `Aduuh! Fitur *${feature}* cuma bisa dipake di dalam grup aja kak~ (｡T ω T｡)`
                 }, { quoted: m });
             }
         }
 
         if (feature === 'welcome') {
             await group.update({ is_welcome: status });
-            await sock.sendMessage(msgData.remoteJid, { 
-                text: `Horeee! Fitur *welcome* sekarang sudah Ryzumi ${status ? 'aktifkan' : 'matikan'} buat grup ini yaa~ (˶˃ ᵕ ˂˶)` 
+            await sock.sendMessage(msgData.remoteJid, {
+                text: `Horeee! Fitur *welcome* sekarang sudah Ryzumi ${status ? 'aktifkan' : 'matikan'} buat grup ini yaa~ (˶˃ ᵕ ˂˶)`
             }, { quoted: m });
         } else if (feature === 'limit') {
             await group.update({ is_limited: status });
-            await sock.sendMessage(msgData.remoteJid, { 
-                text: `Selesai kak! Sekarang fitur *limit* di grup ini sudah ${status ? 'aktif' : 'mati'}~ ${status ? 'Sekarang setiap command bakal ngurangin limit lagi yaa.. (｡T ω T｡)' : 'Horeee! Sekarang kakak bebas pakai command apa aja tanpa limit! (๑>ᴗ<๑)'}` 
+            await sock.sendMessage(msgData.remoteJid, {
+                text: `Selesai kak! Sekarang fitur *limit* di grup ini sudah ${status ? 'aktif' : 'mati'}~ ${status ? 'Sekarang setiap command bakal ngurangin limit lagi yaa.. (｡T ω T｡)' : 'Horeee! Sekarang kakak bebas pakai command apa aja tanpa limit! (๑>ᴗ<๑)'}`
             }, { quoted: m });
         }
     }
